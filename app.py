d5274ac55e7d31c293e3479354b8d47ebc602d10
PurpleZenith <purplezenith727@gmail.com>
2023-07-13 17:31:57 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,3 +7,24 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.model_selection import train_test_split
 from datetime import datetime
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+st.title('Sell with ML :house:')
+tab1, tab2, tab3 = st.tabs(["Data Input", "Price Factors", "Recommendations and Visualizations"])
+
+
+
